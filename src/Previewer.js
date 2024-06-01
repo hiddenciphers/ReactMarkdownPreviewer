6c192e54b6e5a4bed4356e011bb2a9ffdc02a773
hiddenciphers <psolvyns@protonmail.com>
2024-06-01 20:26:19 +1000

    Removed markdown cdn script and installed m marked via npm then added state logic to handle editor input change and preview markdown display

--- src/Previewer.js
+++ src/Previewer.js
@@ -1,22 +1,45 @@
+import React, { useState } from 'react';
+import { marked } from 'marked';
 import './Previewer.css';
 
 function Previewer() {
+  const [markdown, setMarkdown] = useState('');
+
+  const handleInputChange = (event) => {
+    setMarkdown(event.target.value);
+  };
+
   return (
     <div id='Previewer'>
       <div id='container'>
         <div id='editor-container'>
-            <div id='editor-top-bar'>
-                <div className='top-bar-icons'><img className='icons' src='./flame_icon.png' alt='freeCodeCamp Flame Icon'/>Editor</div>
-                <div><img className='icons' src='./cross.png' alt='Quit Icon'/></div>
+          <div id='editor-top-bar'>
+            <div className='top-bar-icons'>
+              <img className='icons' src='./flame_icon.png' alt='freeCodeCamp Flame Icon' />Editor
             </div>
-            <textarea id='editor'></textarea>
+            <div>
+              <img className='icons' src='./cross.png' alt='Quit Icon' />
+            </div>
+          </div>
+          <textarea
+            id='editor'
+            value={markdown}
+            onChange={handleInputChange}
+          ></textarea>
         </div>
         <div id='preview-container'>
-            <div id='preview-top-bar'>
-                <div className='top-bar-icons'><img className='icons' src='./flame_icon.png' alt='freeCodeCamp Flame Icon'/>Previewer</div>
-                <div><img className='icons' src='./cross.png' alt='Quit Icon'/></div>
+          <div id='preview-top-bar'>
+            <div className='top-bar-icons'>
+              <img className='icons' src='./flame_icon.png' alt='freeCodeCamp Flame Icon' />Previewer
             </div>
-            <div id='preview'></div>
+            <div>
+              <img className='icons' src='./cross.png' alt='Quit Icon' />
+            </div>
+          </div>
+          <div
+            id='preview'
+            dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+          ></div>
         </div>
       </div>
     </div>
